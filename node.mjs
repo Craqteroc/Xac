11e150d5df4b25cde6538e5c64a8e315fcfc416c
Craqteroc <Craqteroc@users.noreply.github.com>
2025-02-12 07:16:54 +0000

    Deploying to gh-pages from @ Craqteroc/Xac@33ef22899bb4b0ccfba1ea732d015023e6126d61 🚀

--- node.mjs
+++ node.mjs
@@ -5590,7 +5590,7 @@ var $;
             event_change(next) {
                 if (!next)
                     return;
-                const el = next.target;
+                const el = this.dom_node();
                 const from = el.selectionStart;
                 const to = el.selectionEnd;
                 try {
