88e029e303e7f1c9b26ba7b1027d85d5726147e0
Craqteroc <Craqteroc@users.noreply.github.com>
2026-03-24 07:54:02 +0000

    Deploying to gh-pages from @ Craqteroc/Xac@33ef22899bb4b0ccfba1ea732d015023e6126d61 🚀

--- web.js
+++ web.js
@@ -6961,6 +6961,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
